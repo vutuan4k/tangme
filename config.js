77c761f77e298d7da50ccd145bc89326c7e13501
vutuan4k <dungdienvuchi@gmail.com>
2024-10-20 11:47:33 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Chúc mẹ 20/10 vv';
+const giftUrl = 'https://www.facebook.com/ZhengYujunwu';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Lời gửi mẹ 20/10';
+const contentCard = 'Chúc mừng ngày Phụ nữ Việt Nam mẹ! Con biết ơn mẹ vì đã luôn yêu thương, chăm sóc và hi sinh cho gia đình. Con mong mẹ luôn mạnh khỏe, hạnh phúc và cười thật nhiều. Mẹ mãi là người phụ nữ tuyệt vời nhất trong lòng con.'
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'VN.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
